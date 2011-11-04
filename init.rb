9bff2c855638a7eac737ebfaf51029f87e3a4cb8
Jose M. Prieto <jmprieto@emergya.com>
2011-11-04 11:31:54 +0100

    l10n: module and permission names

--- init.rb
+++ init.rb
@@ -10,5 +10,5 @@ Redmine::Plugin.register :redmine_xmi_import do
   project_module :xmi_import do
     permission :xmi_import, :xmi_import => :index
   end
-  menu :project_menu, :xmi_import, { :controller => 'xmi_import', :action => 'index' }, :caption => :label_import, :before => :settings, :param => :project_id
+  menu :project_menu, :xmi_import, { :controller => 'xmi_import', :action => 'index' }, :caption => :label_xmi_import, :before => :settings, :param => :project_id
 end
