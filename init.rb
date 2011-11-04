daff05a92b2fab69fd0716e918081ae9e66ace20
Jose M. Prieto <jmprieto@emergya.com>
2011-11-04 13:05:08 +0100

    fix: permissions error on match

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_xmi_import do
   url 'http://www.emergya.es'
 
   project_module :xmi_import do
-    permission :xmi_import, :xmi_import => :index
+    permission :xmi_import, :xmi_import => [:index, :match]
   end
   menu :project_menu, :xmi_import, { :controller => 'xmi_import', :action => 'index' }, :caption => :label_xmi_import, :before => :settings, :param => :project_id
 end
